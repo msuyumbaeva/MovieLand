1a37f7b6c2a73d59a1d0b76f29fc0604d25c44d0
msuyumbaeva <msuyumbaeva@gmail.com>
2020-07-29 16:57:57 +0600

    Created ControllerBase

---
+++ MovieLand/Controllers/ControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieLand.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieLand.Controllers
+{
+    public class ControllerBase : Controller
+    {
+        protected void AddErrors(ICollection<string> errors) {
+            foreach (var error in errors) {
+                ModelState.AddModelError(string.Empty, error);
+            }
+        }
+    }
+}
