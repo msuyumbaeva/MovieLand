c5c7e165b6dde882d0cefc075f8e9bc1e179b431
msuyumbaeva <msuyumbaeva@gmail.com>
2020-08-03 09:24:28 +0600

    Added login

---
+++ MovieLand/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MovieLand.Data.Models;
+using MovieLand.ViewModels.Account;
+
+namespace MovieLand.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                await _signInManager.SignOutAsync();
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                    return RedirectToLocal(returnUrl);
+                else
+                    ModelState.AddModelError("", "Invalid cridentials");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        #region Helpers        
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+
+        #endregion
+
+    }
+}
