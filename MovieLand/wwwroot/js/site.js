01b469f49029d6fe7b79211527c6dd45dd57c4d1
msuyumbaeva <msuyumbaeva@gmail.com>
2020-08-05 10:52:36 +0600

    Took choices and datatables scripts out to partials

--- MovieLand/wwwroot/js/site.js
+++ MovieLand/wwwroot/js/site.js
@@ -2,14 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-const formatArrayForChoices = function (array) {
-    const newArray = new Array()
-    array.map(a => {
-        newArray.push({
-            "value": a["id"],
-            "label": a["name"]
-        })
-    })
-    return newArray;
-}
