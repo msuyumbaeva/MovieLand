f42a9cf99f0beea744a66718e2293e64a20c8a47
msuyumbaeva <msuyumbaeva@gmail.com>
2020-08-05 12:01:17 +0600

    Took genre index js part out

---
+++ MovieLand/wwwroot/js/datatablesClient.js
@@ -0,0 +1,39 @@
+﻿function setDataTable(dataUrl, editUrl, columns) {
+    if (editUrl) {
+        columns.push({
+            searchable: false,
+            orderable: false,
+            render: function (data, type, full, meta) {
+                return `<a onClick="editdata('${editUrl},${full.Id}')" href="javascript://">Edit</a>`;
+            }
+        })
+    }
+
+    return $("#data-table").DataTable({
+        // Design Assets
+        stateSave: true,
+        autoWidth: true,
+        // ServerSide Setups
+        processing: true,
+        serverSide: true,
+        // Paging Setups
+        paging: true,
+        // Searching Setups
+        searching: { regex: true },
+        ajax: {
+            url: dataUrl,
+            type: 'POST',
+            contentType: "application/json; charset=utf-8",
+            data: function (d) {
+                return JSON.stringify(d);
+            }
+        },
+        columns,
+        order: [[1, 'asc']],
+        responsive: true
+    });
+}
+
+function editdata(url, id) {
+    window.location.replace(url + `/${id}`)
+}
---
+++ MovieLand/wwwroot/js/genreIndex.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(async function () {
+    const dataUrl = '/Genre/LoadTable';    
+    const editUrl = '/Genre/Edit';
+    const columns = [
+        { data: 'Id', visible: false, searchable: false, orderable: false },
+        { data: 'Name', name: "Name" }
+    ]
+
+    const table = setDataTable(dataUrl, editUrl, columns)
+})
+
