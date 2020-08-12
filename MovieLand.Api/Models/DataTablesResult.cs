4c0c19d78903004667342da7e9a7a60581ed94c8
msuyumbaeva <msuyumbaeva@gmail.com>
2020-08-12 18:27:48 +0600

    Created api project

---
+++ MovieLand.Api/Models/DataTablesResult.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieLand.Api.Models
+{
+    public class DataTablesResult<T>
+    {
+        public DataTablesResult() { }
+
+        [JsonProperty(PropertyName = "draw")]
+        public int Draw { get; set; }
+        [JsonProperty(PropertyName = "recordsTotal")]
+        public int RecordsTotal { get; set; }
+        [JsonProperty(PropertyName = "recordsFiltered")]
+        public int RecordsFiltered { get; set; }
+        [JsonProperty(PropertyName = "data")]
+        public IEnumerable<T> Data { get; set; }
+    }
+}
---
+++ MovieLand.Api/Startup.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieLand.BLL;
+using MovieLand.BLL.Configurations;
+using MovieLand.BLL.Contracts;
+using MovieLand.BLL.Services;
+using MovieLand.Data.ApplicationDbContext;
+using MovieLand.Data.Contracts.Repositories;
+using MovieLand.Data.Repositories;
+
+namespace MovieLand.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+            swwwRootPath = @"C:\Users\E7450\source\repos\MovieLand\MovieLand\wwwroot\";
+        }
+
+        public IConfiguration Configuration { get; }
+        public string swwwRootPath { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.Configure<CookiePolicyOptions>(options => {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Setup db connection
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseSqlServer(connection);
+            });
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(connection);
+
+            // Setup entity services
+            services.AddTransient<IMovieService, MovieService>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<IFileClient, LocalFileClient>(client => {
+                return new LocalFileClient(swwwRootPath);
+            });
+
+            // Setup custom configurations
+            var moviePosterFileConfiguration = Configuration.GetSection("MoviePosterFileConfiguration");
+            services.Configure<MoviePosterFileConfiguration>(moviePosterFileConfiguration);
+
+            // Setup mapping profiles
+            var mappingConfig = new MapperConfiguration(mc => {
+                mc.AddProfile(new MappingProfileBLL());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+            else {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
