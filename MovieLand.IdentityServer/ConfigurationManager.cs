08c8a5f4af0f45f4661ef406e433e20cd797942a
msuyumbaeva <msuyumbaeva@gmail.com>
2020-08-13 18:49:39 +0600

    Created IdentityServer4 server

---
+++ MovieLand.IdentityServer/ConfigurationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieLand.IdentityServer
+{
+    public class ConfigurationManager
+    {
+        public string Hash { get; set; }
+        public string hgAuthSecret { get; set; }
+        public string AppSecret { get; set; }
+        public string defaultAdminPsw { get; set; }
+        public string ClientId { get; set; }
+        public string ApiName { get; set; }
+    }
+}
---
+++ MovieLand.IdentityServer/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MovieLand.Data.ApplicationDbContext;
+using MovieLand.Data.Models;
+using IdentityServer4;
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace MovieLand.IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services) {
+
+            // Setup db connection
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseSqlServer(connection);
+            });
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(connection);
+
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            var builder = services.AddIdentityServer(options => {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+               .AddInMemoryIdentityResources(Config.GetIdentityResources())
+               .AddInMemoryApiResources(Config.GetApis())
+               .AddInMemoryClients(Config.GetClients())
+               .AddAspNetIdentity<AppUser>();
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.Run(async (context) => {
+                await context.Response.WriteAsync("IdSrv4");
+            });
+        }
+    }
+}
